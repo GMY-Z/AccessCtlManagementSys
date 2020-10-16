70bc9a292bde20e3b18a1c739a3dd7eb6e46b86c
Guanmuyang <1321879427@qq.com>
2020-10-16 10:51:26 +0800

    init

---
+++ src/main/java/com/gmy/AccessCTLManagementSys/AccessCtlManagementSysApplication.java
@@ -0,0 +1,13 @@
+package com.gmy.AccessCTLManagementSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessCtlManagementSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessCtlManagementSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmy/AccessCTLManagementSys/AccessCtlManagementSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmy.AccessCTLManagementSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessCtlManagementSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
